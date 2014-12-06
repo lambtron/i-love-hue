3944f23c4ebdda1819c331b95333396fb52cdd02
Andy Jiang <andyjiang@gmail.com>
2014-12-06 15:00:55 -0800

    moved from main to index for game logic

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ I :heart: Hue
 
 [LDJAM #31](http://ludumdare.com/compo/) entry, "I :heart: Hue".
 
-Game logic is in `./public/lib/main.js`. Uses [pixi.js](http://www.pixijs.com/) for image rendering.
+Game logic is in `./public/js/index.js`. Uses [pixi.js](http://www.pixijs.com/) for image rendering.
 
 Requires `node 0.11.x` to run. To run locally, just run `make`.
---
+++ public/js/index.js
@@ -0,0 +1,35 @@
+
+/**
+ * Main game logic.
+ */
+
+/**
+ * 'Collision detection' is if the colors and filters match.
+ */
+
+/**
+ * `Obstacle` factory.
+ */
+
+/**
+ * `Obstacle` object.
+ *
+ * @property {Array} filters i.e. ['1', '2', ... , '3']
+ * @property {Object} shape
+ * @property {Function} draw
+ */
+
+/**
+ * `Filter` factory.
+ */
+
+/**
+ * `Filter` object.
+ *
+ * @property {Integer} id
+ * @property {Function} draw
+ */
+
+/**
+ * Obstacle queue.
+ */
